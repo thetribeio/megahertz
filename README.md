2fc89f78a0a4e630152b938317d6939b28d68165
Harold Cohen <harold.cohen@thetribe.io>
2023-09-18 14:16:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MegaHertz
+
+This repository is the support project for the software architecture course from theTribe Academy.
---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+    ],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[jt]sx?$': 'ts-jest',
+    },
+};
